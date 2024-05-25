99ed5b71d9f8fc70a053d825684ebaff5d441e49
sagar-7227 <sagarvashnav72427@gmail.com>
2024-05-25 18:32:01 +0530

    apache-kafka

---
+++ cab-book-driver/src/main/java/com/example/cabbookdriver/config/KafkaConfig.java
@@ -0,0 +1,20 @@
+package com.example.cabbookdriver.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import com.example.cabbookdriver.constant.AppConstant;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder
+                .name(AppConstant.CAB_LOCATION)
+                .build();
+    }
+
+}
